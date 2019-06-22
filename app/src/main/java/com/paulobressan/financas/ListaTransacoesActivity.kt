d466d34b344edee6528e0d7cbe63760efa79a155
paulobressan <paulo.bressan@outlook.com>
2019-06-22 18:35:41 -0300

    Iniciando Projeto, criando layout.

---
+++ app/src/main/java/com/paulobressan/financas/ListaTransacoesActivity.kt
@@ -0,0 +1,12 @@
+package com.paulobressan.financas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+    }
+}
